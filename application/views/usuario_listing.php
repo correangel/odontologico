8fe1333abb785580dbb6e20f3a1b723b9e24b570
Marcos <mvpdev@gmail.com>
2014-11-28 00:44:38 -0200

    Remoção de referências ao plugin dataTable. Inclusão do plugin bootGrid.

--- application/views/usuario_listing.php
+++ application/views/usuario_listing.php
@@ -3,26 +3,57 @@
 </div>
 <br>
 <div class="table-responsive">
-	<table id="tabela" class="table table-striped table-bordered table-hover">
+	<table id="tabela"
+		class="table table-condensed table-hover table-striped">
 		<thead>
 			<tr>
-				<th>Editar</th>
-				<th>Inativar</th>
-				<th>Login</th>
-				<th>Perfil</th>
+				<th data-column-id="commands" data-formatter="commands"
+					data-sortable="false">Opções</th>
+				<th data-column-id="id" data-type="numeric">ID</th>
+				<th data-column-id="login" data-order="asc">Login</th>
+				<th data-column-id="perfil">Perfil</th>
 			</tr>
 		</thead>
 		<tbody>
-			<?php 
-				foreach($usuarios->result() as $usuario):
-					echo '<tr>';
-					echo '<td>' . anchor('usuario/edit/' . $usuario->id_usuario, '<span class="glyphicon glyphicon-pencil"></span>') . '</td>';
-					echo '<td>' . anchor('usuario/inativa/' . $usuario->id_usuario, '<span class="glyphicon glyphicon-minus"></span>') . '</td>';
-					echo '<td>' . $usuario->login . '</td>';
-					echo '<td>' . $usuario->nome_perfil . '</td>';
-					echo '</tr>';
-				endforeach;			
+			<?php
+			foreach ( $usuarios->result () as $usuario ) :
+				echo '<tr>';
+				echo '<td></td>';
+				echo '<td>' . $usuario->id_usuario . '</td>';
+				// echo '<td>' . anchor('usuario/edit/' . $usuario->id_usuario, '<span class="glyphicon glyphicon-pencil"></span>') . '</td>';
+				// echo '<td>' . anchor('usuario/inativa/' . $usuario->id_usuario, '<span class="glyphicon glyphicon-minus"></span>') . '</td>';
+				echo '<td>' . $usuario->login . '</td>';
+				echo '<td>' . $usuario->nome_perfil . '</td>';
+				echo '</tr>';
+			endforeach
+			;
 			?>
 	  </tbody>
 	</table>
 </div>
+
+<script>
+$(document).ready(function(){ 
+	var grid = $("table").bootgrid({
+		formatters: {
+        "commands": function(column, row)
+        {
+            return "<button type=\"button\" class=\"btn btn-xs btn-default command-edit\" data-row-id=\"" + row.id + "\"><span class=\"glyphicon glyphicon-pencil\"></span></button> " + 
+                "<button type=\"button\" class=\"btn btn-xs btn-default command-delete\" data-row-id=\"" + row.id + "\"><span class=\"glyphicon glyphicon-trash\"></span></button>";
+        }
+    }
+}).on("loaded.rs.jquery.bootgrid", function()
+{
+
+    /* Executes after data is loaded and rendered */
+    grid.find(".command-edit").on("click", function(e)
+    {
+    	//window.location("http://localhost/odontologico/index.php/usuario/edit/" + $(this).data("data-row-id"));
+    	window.location.replace("edit/" + $(this).data("row-id"));
+    }).end().find(".command-delete").on("click", function(e)
+    {
+    	window.location.replace("inativa/" + $(this).data("row-id"));
+    });
+});
+});
+</script>
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,50 +1,9 @@
-$(document).ready(function(){ 
-		$("table").dataTable( {
-			"dom": 'T<"clear">lfrtip', 
-			"tableTools": {
-				"aButtons": [
-					{
-						"sExtends": "copy",
-						"sButtonText": "Copiar"								
-					},
-					{
-						"sExtends": "csv",
-						"sButtonText": "CSV"
-					},
-					{
-						"sExtends": "xls",
-						"sButtonText": "Excel"
-					}, 
-					{
-						"sExtends": "pdf", 
-						"sPdfSize": "A4", 
-						"sButtonText": "PDF"
-					}, 
-					{
-						"sExtends": "print", 
-						"sButtonText": "Imprimir"
-					}
-				], 
-				"sSwfPath": "../../assets/swf/copy_csv_xls_pdf.swf"
-			},
-		} );
 
-}); 
 
 jQuery(function($){
    $(".cnpj").mask("99.999.999-9999/99");
+   $(".cpf").mask("999.999.999-99");
    $(".telefone").mask("(99) 9999-9999");
-   $(".data_nota").mask("99/99/9999");
+   $(".data_nascimento").mask("99/99/9999");
 });
-
-$(function() {
-	$("#data_nota").datepicker();
-	$("#data_inicial").datepicker();
-	$("#data_final").datepicker();
-});
-
-jQuery(function($){
-	$("input").tooltip({ placement: "right" });
-});
-
 

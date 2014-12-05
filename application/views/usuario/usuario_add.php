0b8255d7fd55521308e70fc4d6794332721b74ef
Marcos <mvpdev@gmail.com>
2014-12-04 23:18:24 -0200

    Mudança de layout nos formulários de cadastro e edição de usuários do sistema.

--- application/views/usuario/usuario_add.php
+++ application/views/usuario/usuario_add.php
@@ -1,31 +1,42 @@
-﻿<form class="form-cadastro" role="form" action="<?php echo base_url('index.php/usuario/create'); ?>" method="post">
+﻿<form role="form" action="<?php echo base_url('index.php/usuario/create'); ?>" method="post">
 
-	<h3 class="form-cadastro-heading"><?php echo $headline ?></h3>
+	<div class="page-header">
+		<h2><?php echo $headline ?></h3>
+	</div>
 	
-	<div class="form-group has-feedback">
-		<label for="login" class="sr-only">Login</label>
-    	<input type="text" id="login" name="login" class="form-control" placeholder="Login" required autofocus>
-    	<span class="glyphicon glyphicon-user form-control-feedback" aria-hidden="true"></span>
+	<div class="row">
+		<div class="form-group col-xs-4">
+			<label for="login">Login</label>
+	    	<input type="text" id="login" name="login" class="form-control" placeholder="Login" required autofocus>
+	    </div>
     </div>
     
-    <div class="form-group has-feedback">
-    	<label for="password" class="sr-only">Senha</label>
-    	<input type="password" id="password" name="senha" class="form-control" placeholder="Senha" required>
-    	<span class="glyphicon glyphicon-lock form-control-feedback" aria-hidden="true"></span>
+    <div class="row">
+	    <div class="form-group col-xs-4">
+	    	<label for="password">Senha</label>
+	    	<input type="password" id="password" name="senha" class="form-control" placeholder="Senha" required>
+	    </div>
     </div>
     
-    <div class="form-group">
-	    <select name="perfil" class="form-control" required>
-	    	<option value="">Escolha um perfil</option>
-	    	<option></option>
-		    <?php 
-			    foreach($perfis->result() as $perfil):
-					echo ('<option value="'.$perfil->id_perfil.'">'.$perfil->nome_perfil.'</option>');
-				endforeach;
-			?>
-	    </select>
+    <div class="row">
+	    <div class="form-group col-xs-4">
+	    	<label for="perfil">Perfil</label>
+		    <select name="perfil" class="form-control" required>
+		    	<option value="">Escolha um perfil</option>
+		    	<option></option>
+			    <?php 
+				    foreach($perfis->result() as $perfil):
+						echo ('<option value="'.$perfil->id_perfil.'">'.$perfil->nome_perfil.'</option>');
+					endforeach;
+				?>
+		    </select>
+		</div>
 	</div>
     
-    <button type="submit" class="btn btn-primary">Cadastrar</button>
+    <div class="row">
+		<div class="col-xs-2">
+    		<button type="submit" class="btn btn-primary">Cadastrar</button>
+    	</div>
+    </div>
 
 </form>
--- application/views/usuario/usuario_edit.php
+++ application/views/usuario/usuario_edit.php
@@ -1,36 +1,47 @@
-﻿<form class="form-cadastro" role="form" action="<?php echo base_url('index.php/usuario/update'); ?>" method="post">
+﻿<form role="form" action="<?php echo base_url('index.php/usuario/update'); ?>" method="post">
 
-	<h3 class="form-cadastro-heading"><?php echo $headline ?></h3>
+	<div class="page-header">
+		<h2><?php echo $headline ?></h3>
+	</div>
 	
 	<input type="hidden" name="id_usuario" value="<?php echo $usuario[0]->id_usuario ?>" >
 	
-	<div class="form-group has-feedback">
-		<label for="login" class="sr-only">Login</label>
-    	<input type="text" id="login" name="login" value="<?php echo $usuario[0]->login; ?>" class="form-control" placeholder="Login" required autofocus>
-    	<span class="glyphicon glyphicon-user form-control-feedback" aria-hidden="true"></span>
+	<div class="row">
+		<div class="form-group col-xs-4">
+			<label for="login">Login</label>
+    		<input type="text" id="login" name="login" value="<?php echo $usuario[0]->login; ?>" class="form-control" placeholder="Login" required autofocus>
+    	</div>
     </div>
     
-    <div class="form-group has-feedback">
-    	<label for="password" class="sr-only">Senha</label>
-    	<input type="password" id="password" name="senha" value="<?php echo $usuario[0]->senha; ?>" class="form-control" placeholder="Senha" required>
-    	<span class="glyphicon glyphicon-lock form-control-feedback" aria-hidden="true"></span>
+    <div class="row">
+    	<div class="form-group col-xs-4">
+    		<label for="password">Senha</label>
+    		<input type="password" id="password" name="senha" value="<?php echo $usuario[0]->senha; ?>" class="form-control" placeholder="Senha" required>
+    	</div>
     </div>
     
-    <div class="form-group">
-	    <select name="perfil" class="form-control" required>
-	    	<option value="">Escolha um perfil</option>
-	    	<option></option>
-		    <?php 
-			    foreach($perfis->result() as $perfil):
-					echo ('<option value="'.$perfil->id_perfil.'"');
-						if($perfil->id_perfil == $usuario[0]->perfil)
-							echo('selected="selected"');
-						echo('>'.$perfil->nome_perfil.'</option>');
-				endforeach;
-			?>
-	    </select>
+    <div class="row">
+	    <div class="form-group col-xs-4">
+	    	<label for="perfil">Perfil</label>
+		    <select name="perfil" class="form-control" required>
+		    	<option value="">Escolha um perfil</option>
+		    	<option></option>
+			    <?php 
+				    foreach($perfis->result() as $perfil):
+						echo ('<option value="'.$perfil->id_perfil.'"');
+							if($perfil->id_perfil == $usuario[0]->perfil)
+								echo('selected="selected"');
+							echo('>'.$perfil->nome_perfil.'</option>');
+					endforeach;
+				?>
+		    </select>
+		</div>
 	</div>
     
-    <button type="submit" class="btn btn-primary">Atualizar</button>
+	<div class="row">
+		<div class="col-xs-2">
+    		<button type="submit" class="btn btn-primary">Atualizar</button>
+    	</div>
+    </div>
 
 </form>
